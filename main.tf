1db445fee0d77309e4b3b2f8270c2e75c0dd086d
shabbirsaifee92 <shabbirsaifee91@gmail.com>
2021-11-02 00:27:57 -0400

    add azure container group through terraform

--- main.tf
+++ main.tf
@@ -17,3 +17,24 @@ resource "azurerm_resource_group" "azure_devops_docker_terrform_rg" {
   location = "centralus"
   name = "azure-devops-docker-terrform-rg"
 }
+
+resource "azurerm_container_group" "azure_devops_docker_terrform_cg" {
+  name = "staticwebapp"
+  location = azurerm_resource_group.azure_devops_docker_terrform_rg.location
+  resource_group_name = azurerm_resource_group.azure_devops_docker_terrform_rg.name
+
+  ip_address_type = "public"
+  dns_name_label = "staticwebapp"
+  os_type = "Linux"
+
+  container {
+    name = "staticwebapp"
+    image = "aksdevopsacrdemo.azurecr.io/dev/staticwebapp"
+    cpu = "1"
+    memory = "1"
+    ports {
+      port = "80"
+      protocol = "TCP"  
+    }
+  }
+}
